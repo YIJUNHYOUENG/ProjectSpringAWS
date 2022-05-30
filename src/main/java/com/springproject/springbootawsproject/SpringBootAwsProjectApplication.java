7dc66584506b8875926908bbeb2713e9c2eec1e2
YiJunHyeong <ljh040325@gmail.com>
2022-05-30 21:00:47 +0900

    Initial commit

---
+++ src/main/java/com/springproject/springbootawsproject/SpringBootAwsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.springbootawsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAwsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springproject/springbootawsproject/SpringBootAwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.springbootawsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
